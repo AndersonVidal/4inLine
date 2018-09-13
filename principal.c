6a3e0e7f6899e828c5623ae4e4e4b2e811421f7f
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-09-12 21:00:34 -0300

    Add principal and stylize header

--- principal.c
+++ principal.c
@@ -1,31 +1,58 @@
+// C STANDARD LIBRARIES
 #include <stdio.h>
+#include <ctype.h>
+
+// PROJECT LIBRARIES
 #include "menu.h"
-#include "register.h"
+#include "createGame.h"
 #include "rules.h"
 #include "about.h"
 
-int main() {
-  printf("4inLine\n");
+#define KGRN  "\x1B[32m"
 
-  printf("Welcome\n");
+// FUNCTION DECLARATIONS
+void manageMenu();
+
+// COLORS DEFINITIONS
+#define NOR  "\x1B[0m"
+#define RED  "\x1B[31m"
+#define GRE  "\x1B[32m"
+#define YEL  "\x1B[33m"
+#define BLU  "\x1B[34m"
+#define MAG  "\x1B[35m"
+#define CYA  "\x1B[36m"
+#define WHI  "\x1B[37m"
+
+int main() {
+
+  printf(CYA "***************************\n");
+  printf(RED "        4 In A Line        \n");
+  printf(CYA "***************************\n" NOR);
+
+  printf(WHI "          Welcome!         \n\n" NOR);
   
+  manageMenu();
+
+  return 0;
+}
+
+// FUNCTION DEFINITIONS
+void manageMenu() {
   showMenu();
 
-  char userChoice;
-  while (userChoice != 'E') {  
+  char userChoice = ' ';
+  while (userChoice != getExitOption()) {  
     scanf("%c", &userChoice); 
-    if (userChoice == 'N') {
-      newGame();
+    userChoice = toupper(userChoice);
+
+    if (userChoice == getCreateGameOption()) {
+      createGame();
     }
-    else if (userChoice == 'R') {
+    else if (userChoice == getRulesOption()) {
       showRules();
     }
-    else if (userChoice == 'A') {
-      showAboutPanel();
-    }
-    else if (userChoice == 'E') {
-      return 0;
+    else if (userChoice == getAboutOption()) {
+      showAboutMsg();
     }
   }
-  return 0;
 }
