35da512f951cb5cf97e312c108a1d80aa57edfa0
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-09-12 21:04:41 -0300

    Add comments

--- principal.c
+++ principal.c
@@ -8,18 +8,16 @@
 #include "rules.h"
 #include "about.h"
 
-#define KGRN  "\x1B[32m"
-
 // FUNCTION DECLARATIONS
 void manageMenu();
 
 // COLORS DEFINITIONS
 #define NOR  "\x1B[0m"
 #define RED  "\x1B[31m"
-#define GRE  "\x1B[32m"
-#define YEL  "\x1B[33m"
-#define BLU  "\x1B[34m"
-#define MAG  "\x1B[35m"
+//#define GRE  "\x1B[32m"
+//#define YEL  "\x1B[33m"
+//#define BLU  "\x1B[34m"
+//#define MAG  "\x1B[35m"
 #define CYA  "\x1B[36m"
 #define WHI  "\x1B[37m"
 
