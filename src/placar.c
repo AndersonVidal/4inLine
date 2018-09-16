f576d19f43fc1ed66b5b51a0a9f54d36049ffef4
Bruno Gregório <Bruno08new12@gmail.com>
2018-09-15 23:21:04 -0300

    Novos arquivos

---
+++ src/placar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int chamarPlacar(int vitorias, int derrotas, int empates){
+  /*
+  esta função deve exibir na tela o placar e duas opções:
+  0 jogar novamente
+  1 voltar ao menu principal
+  retornar 0 caso seja pra ser jogado novamente
+  retornar 1 caso seja para voltar ao menu principal
+  */
+
+}
