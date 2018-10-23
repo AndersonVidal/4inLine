dc063ad7cc4a9d0fac912bfb0ebd53584a66ffc7
anderson.vidal <anderson.vidal@virtus.ufcg.edu.br>
2018-10-23 13:47:48 -0300

    read files

---
+++ haskell/interface.hs
@@ -0,0 +1,6 @@
+
+g = (6.67 * (10^^(-11)))
+
+forcaAtracao :: Double -> Double -> Double -> Double
+forcaAtracao m1 m2 d = ((m1 * m2) * g) / (d ^^
+ 2)
--- haskell/src/menu.hs
+++ haskell/src/menu.hs
@@ -1,23 +1,36 @@
-import Control.Monad  
-import Data.Char  
+import System.IO  
+import Control.Monad
 
-menu :: Char -> [Char]
-menu entrada | (entrada == 'B') = "Aqui está o about.txt"
-             | (entrada == 'R') = "Aqui está a rules.txt"
-             | (entrada == 'A') = "Aqui está a apresentation.txt"
-             | (entrada == 'E') = "Aqui está o goodby.txt"
-             | otherwise = "Entrada inválida!!"
 
-compute_entrada :: Char -> [Char]
+menu entrada | (entrada == 'B') = do
+                let x = readFile "../files/About.txt"
+                let t = show x
+                return(t)
+             | (entrada == 'R') = do
+                let x = readFile "../files/Rules.txt"
+                let t = show x
+                return(t)
+             | (entrada == 'A') = do 
+                let x = readFile "../files/Apresentation.txt"
+                let t = show x
+                return(t)
+             | (entrada == 'E') = do 
+                let x = readFile "../files/Goodby.txt"
+                let t = show x
+                return(t)
+             | (entrada == 'M') = do 
+                let x = readFile "../files/Menu.txt"
+                let t = show x
+                return(t)
+             | otherwise = do
+                let t = "Opção invalida!!!"
+                return(t)
+
 compute_entrada entrada | (entrada == '1') = menu 'B'
                         | (entrada == '2') = menu 'R'
                         | (entrada == '3') = menu 'A'
                         | (entrada == '4') = menu 'E'
                         | otherwise = menu '0'
-
-imprime_menu :: [Char]
-imprime_menu = do
-
 
 main :: IO()
 main = do
