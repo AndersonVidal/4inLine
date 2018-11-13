530174351ed4478579948663d36d9b31408dc36f
gabialverga <gabrielaalverga@gmail.com>
2018-11-13 01:16:02 -0300

    ”

--- prolog/src/menu.pl
+++ prolog/src/menu.pl
@@ -12,3 +12,17 @@ callFile(Opt):- Opt == 0 -> writeln("+---------------------------+
 
 main:- callFile(0),
        halt(0).
+
+    % main :-
+    %     open('myFile.txt', read, Str),
+    %     read_file(Str,Lines),
+    %     close(Str),
+    %     write(Lines), nl.
+    
+    % read_file(Stream,[]) :-
+    %     at_end_of_stream(Stream).
+    
+    % read_file(Stream,[X|L]) :-
+    %     \+ at_end_of_stream(Stream),
+    %     read(Stream,X),
+    %     read_file(Stream,L).
