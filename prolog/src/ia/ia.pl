cb0133a38202d2ca0ccf67926e173c969648f77a
Daniele Silva <daniele.silva@ccc.ufcg.edu.br>
2018-11-19 22:33:22 -0300

    Adicionada busca vertical

--- prolog/src/ia/ia.pl
+++ prolog/src/ia/ia.pl
@@ -19,6 +19,28 @@ slice([X|Xs],1,K,[X|Ys]) :- K > 1,
 slice([_|Xs],I,K,Ys) :- I > 1, 
    I1 is I - 1, K1 is K - 1, slice(Xs,I1,K1,Ys).
 
+% Retorna a matriz transposta
+transpose([], []).
+transpose([F|Fs], Ts) :-
+    transpose(F, [F|Fs], Ts).
+
+transpose([], _, []).
+transpose([_|Rs], Ms, [Ts|Tss]) :-
+        lists_firsts_rests(Ms, Ts, Ms1),
+        transpose(Rs, Ms1, Tss).
+
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+        lists_firsts_rests(Rest, Fs, Oss).
+
+
+% Retorna a quantidade de elementos na lista
+len([], LenResult):-
+    LenResult is 0.
+len([X|Y], LenResult):-
+    len(Y, L),
+    LenResult is L + 1.
+
 % Verifica se há possibilidade de jogada na lista e retorna o index
  verifica(Lista, Player, Qtd, Index) :-
     count(Lista, Player, ReturnQtd),
@@ -37,6 +59,21 @@ percorre([L|Ls], Player, Qtd, Count, Index) :-
     Count1 is Count - 1,
     percorre(Ls, Player, Qtd, Count1, Index).
 
+implBuscaVertical([], Player, Qtd, Index) :- Index is -1.
+implBuscaVertical([T|Ts], Player, Qtd, Index) :-
+    Qtd2 is 6-Qtd,
+    percorre(T, Player, Qtd, Qtd2, Indexp),
+    Indexp \= -1,
+    len([T|Ts], Len),
+    Index is 7 - Len;
+    implBuscaVertical(Ts, Player, Qtd, Index).
+
+buscaVertical(Tabuleiro, Player, Qtd, Index) :-
+    transpose(Tabuleiro, Matrix),
+    writeln(Matrix),
+    implBuscaVertical(Matrix, Player, Qtd, Indexb),
+    Index is Indexb.
+
 % Percorre o tabuleiro linha por linha e passa a linha para o percorre
 buscaHorizontal([], Player, Qtd, Index) :- Index is -1.
 buscaHorizontal([T|Ts], Player, Qtd, Index) :-
@@ -48,12 +85,12 @@ buscaHorizontal([T|Ts], Player, Qtd, Index) :-
     
 
 main:-
-    buscaHorizontal([
+    buscaVertical([
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,1,0,1,1,0,0],
+    [1,0,0,0,0,0,0],
+    [1,0,0,0,1,0,0],
     [1,1,1,1,1,1,1]
     ], 1, 3, Index),
     writeln(Index).
